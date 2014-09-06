d4009ef83b3363210f1ca3fcac31e4e4445ae623
Denys Kurets <denys281@gmail.com>
2014-09-06 18:21:59 +0300

    add photo to project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140906151207) do
+ActiveRecord::Schema.define(version: 20140906152124) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140906151207) do
     t.string   "place"
     t.string   "video"
     t.integer  "user_id"
+    t.string   "photo"
   end
 
   add_index "projects", ["user_id"], name: "index_projects_on_user_id"
