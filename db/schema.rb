be3f8b978a6db74440e67ce76f9d1cef0f2c9720
Denys Kurets <denys281@gmail.com>
2014-09-06 18:17:21 +0300

    add ref user to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140906151004) do
+ActiveRecord::Schema.define(version: 20140906151207) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
@@ -22,7 +22,10 @@ ActiveRecord::Schema.define(version: 20140906151004) do
     t.datetime "updated_at"
     t.string   "place"
     t.string   "video"
+    t.integer  "user_id"
   end
+
+  add_index "projects", ["user_id"], name: "index_projects_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :string(255)
+#  status      :string(255)
+#  date_to     :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#  place       :string(255)
+#  video       :string(255)
+#  user_id     :integer
+#
+
 require 'rails_helper'
 
 RSpec.describe Project, :type => :model do
